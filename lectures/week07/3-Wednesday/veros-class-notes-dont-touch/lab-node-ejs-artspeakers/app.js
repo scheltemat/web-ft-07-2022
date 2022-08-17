d8aa8c9d27341ba383006fb4dd9f4636d8a05e86
Veronica Lino <Veronica@digitalcrafts.com>
2022-08-17 13:48:26 -0500

    speakers

---
+++ lectures/week07/3-Wednesday/veros-class-notes-dont-touch/lab-node-ejs-artspeakers/app.js
@@ -0,0 +1,25 @@
+// install express
+const express = require('express');
+const app = express();
+let port = 3000;
+
+// req => express => res
+// configure public folder
+app.use(express.static('public'))// static assests go
+
+//install ejs templates
+app.set('view engine', 'ejs')
+
+
+// setup a routes folder 
+//index.js 
+//speakers.js
+
+app.use(require('./routes/index'))
+app.use(require('./routes/speakers'))
+
+//start a server
+
+app.listen(port, ()=>{
+    console.log(`listenig on port: ${port}`);
+})
---
+++ lectures/week07/3-Wednesday/veros-class-notes-dont-touch/lab-node-ejs-artspeakers/routes/speakers.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/speakers', (req, res) => { 
+    
+
+    res.render('speakers')
+ })
+
+module.exports = router;
