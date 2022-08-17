61bb4e0324e8b922b5f4c2585fe02ac1fe0c232e
Veronica Lino <Veronica@digitalcrafts.com>
2022-08-17 11:18:52 -0500

    lecture notes

--- lectures/week07/3-Wednesday/veros-class-notes-dont-touch/templates/app.js
+++ lectures/week07/3-Wednesday/veros-class-notes-dont-touch/templates/app.js
@@ -7,28 +7,24 @@ let port = 3000;
 //setting a folder that holds static resources 
 
 app.use(express.static('public')) //hold all of our static resources
+// configuring ejs to work with application
+app.set('view engine', 'ejs')  //telling express what templating engine you're using 
+
+// app.set('views', 'myviews')  //maps default views name to something else
 
 app.use(require('./routes/index'))
-
-// app.get('/', (req, res) => { 
-    
-//     res.send('Hello world')
-
-// })
+app.use(require('./routes/aboutus'))
+app.use(require('./routes/'))
+app.use(require('./routes/city'))
 
 
-app.get('/aboutus', (req, res) => { 
-    
-    res.send('About us')
+//n-tier 
+// view layer - buisness layer - data tier
 
-})
+// MVC
+// Model - Viewer - Controller 
 
 
-app.get('/faq', (req, res) => { 
-    
-    res.send('faq')
-
-})
 
 //turn on server 
 
--- lectures/week07/3-Wednesday/veros-class-notes-dont-touch/templates/routes/index.js
+++ lectures/week07/3-Wednesday/veros-class-notes-dont-touch/templates/routes/index.js
@@ -5,7 +5,17 @@ const router = express.Router();  //sub route
 
 router.get('/', (req, res) => { 
 
-    res.send('home page')
+    // res.send('home page')
+
+    res.render('index', {
+        firstName : "Veronica", 
+        lastName:  "Lino", 
+        year: 2022, 
+        nums: [1, 2, 3, 4], 
+        contacts: {year: 2022}
+    })  //rendering a view (template)
+    // second argument of render is how we pass information to our template 
+    // strings, int, bool, objects, arrays, functions
 })
 
 
